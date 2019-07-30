f2d063a4b0e2c0465235dc0d8cf8d8b3e38ec821
patelkarishma10 <patelkarishma10@gmail.com>
2019-07-30 10:47:42 +0100

    created a form in the user component

--- src/User/User.js
+++ src/User/User.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import UserDetail  from './UserDetail';
 
 export class User extends Component {
-    
+    constructor(){
+        super();
+        this.state = {
+            location:"Salford"
+        };
+    }
+
+    newLocation = (e) => {
+e.preventDefault();
+    this.setState ({
+        location: document.getElementById("locationInput").value
+    })
+}
+
     myUserData = {
-        name : "Joe"
+        name : "Joe",
+        location: "Salford"
     };
     render() {
         return (
-        <div><UserDetail userData={this.myUserData}/></div>
+        <div><UserDetail userData={this.myUserData}/>
+        <form onSubmit={this.newLocation}>
+            <label for="locationInput">Enter new location: </label>
+            <input id="locationInput" type= "text" />
+            <input type = "submit" value="set new location"/>
+            </form>
+            <p>The location is: {this.state.location.toString()}</p>
+        </div>
         
     );
     }
